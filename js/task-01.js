9e9f46e880b4c24320528a6fe26a4a931ed79699
Lebed Yulia <YuLebed@ukr.net>
2022-07-07 10:46:26 +0300

    Task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,11 @@
-const categoryNumbers = document.querySelectorAll(".item").length;
-console.log("Number of categories:", categoryNumbers);
+const categories = document.querySelectorAll(".item");
+console.log("Number of categories:", categories.length);
 
+
+categories.forEach(function (item) {
+ const categoryName = item.querySelector("h2");
+ console.log('Category:', categoryName.textContent);
+
+ const categoryEl = item.querySelectorAll("li");
+ console.log('Elements:', categoryEl.length);
+})
