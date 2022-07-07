5a55aca57bcbb4f7641be0a59a364691526a2e21
Lebed Yulia <YuLebed@ukr.net>
2022-07-07 12:41:16 +0300

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+const ingredientsEl = ingredients.map(ingredient => {
+  const element = document.createElement('li');
+  element.classList.add('item');
+  element.textContent = ingredient;
+  return element;
+}); 
+
+console.log(ingredientsEl);
+
+list.append(...ingredientsEl);
