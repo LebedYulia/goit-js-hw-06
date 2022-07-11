1ef82d23a33e360c2b7f430e18b7ef2750c34b69
Lebed Yulia <YuLebed@ukr.net>
2022-07-11 14:44:21 +0300

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const btnChangeColor = document.querySelector('.change-color');
+const hexColorValue = document.querySelector('.color');
+const bodyRef = document.querySelector('body');
+
+
+btnChangeColor.addEventListener('click', () => {
+  bodyRef.style.background = getRandomHexColor();
+  hexColorValue.textContent = bodyRef.style.background;  
+})  
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
