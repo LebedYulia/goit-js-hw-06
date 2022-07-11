f645b06a8a822d11aeecfaf11ffe9867062ba9a2
Lebed Yulia <YuLebed@ukr.net>
2022-07-11 13:00:38 +0300

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const formEl = event.currentTarget.elements;
+  const email = formEl.email.value;
+  const password = formEl.password.value;
+  
+  const formData = {
+    email,
+    password,
+  }
+
+  if (email === "" || password === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
