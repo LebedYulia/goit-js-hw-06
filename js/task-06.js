7162d5472a64b00e88c816cd64b85a168a13bd8c
Lebed Yulia <YuLebed@ukr.net>
2022-07-14 11:16:51 +0300

    Fixed Task 6

--- js/task-06.js
+++ js/task-06.js
@@ -2,14 +2,13 @@ const inputData = document.querySelector('#validation-input');
 const dataLength = inputData.getAttribute('data-length');
 
 inputData.addEventListener('blur', () => {
-    if (event.currentTarget.value.length < dataLength) {  
-        inputData.classList.remove('valid');           
-        inputData.classList.add('invalid');        
-        console.log(inputData);   
-
-    } if  (event.currentTarget.value.length >= dataLength) {
+    if  (event.currentTarget.value.length == dataLength) {
         inputData.classList.remove('invalid');          
         inputData.classList.add('valid');
         console.log(inputData);   
     }
+    else {           
+        inputData.classList.add('invalid');        
+        console.log(inputData);
+    } 
 })
